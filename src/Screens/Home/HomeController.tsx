fd5b3070e7834c07c9bf27667ba9b596605e463f
dgaguiar <daiannegaguiar@gmail.com>
2022-10-15 16:12:47 -0300

    include files

---
+++ src/Screens/Home/HomeController.tsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+import useAPI from "../../Services/APIs/Common/useAPI";
+import persons from "../../Services/APIs/Persons/Persons";
+import HomeView from "./HomeView";
+
+export default function HomeController() {
+  const [count, setCount] = useState(0);
+  const getPersonsGetAPI = useAPI(persons.getPersons);
+  const getPersonsPostAPI = useAPI(persons.getPersonsPost);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      //functional update
+      setCount((count) => count + 1);
+    }, 3000);
+
+    getPersonsGetAPI.request(1);
+    getPersonsPostAPI.request({
+      title: "L",
+    });
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  // console.log(getPersonsGetAPI.data);
+  // console.log(getPersonsPostAPI.data);
+
+  return <HomeView info={count} person={getPersonsGetAPI.data} />;
+}
---
+++ src/Screens/Home/HomeView.tsx
@@ -0,0 +1,61 @@
+import * as React from "react";
+import Grid from "@mui/material/Grid";
+import { AllPersons } from "../../Models/Person";
+import { Title, Main } from "./HomeStyle";
+import { Button, TextField } from "@mui/material";
+
+interface IProps {
+  info: number;
+  person: AllPersons | null;
+}
+
+const inputLoginProps = {
+  step: 300,
+};
+
+const inputSenhaProps = {
+  step: 300,
+};
+
+const HomeView = ({ info, person }: IProps) => { 
+  return (
+    <Main>
+      <Grid
+        container
+        spacing={0}
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Title gutterBottom variant="h1" color="primary.dark">
+            Trabalho Final 24MOB 
+        </Title>
+        <Title gutterBottom variant="h3" color="primary.dark">
+          Login
+        </Title>
+        <TextField 
+          placeholder="login"
+          inputProps={inputLoginProps} 
+        />
+        <Title gutterBottom variant="h3" color="primary.dark">
+          Senha
+        </Title>
+        <TextField 
+          placeholder="senha" 
+          id="senha"
+          inputProps={inputSenhaProps} 
+        />
+        <hr></hr>
+        <Button 
+          variant="outlined" 
+          onClick={() => {
+              alert('clicou em logar');
+          }}>
+          Logar
+        </Button>
+      </Grid>
+    </Main>
+  );
+};
+
+export default HomeView;
